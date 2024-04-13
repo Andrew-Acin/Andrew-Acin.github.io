1e9f0fcc40970f04360d00d67697d55d1a114e9d
Andrew Acin <andrew_acin@hotmail.com>
2024-04-13 11:17:13 -0700

    chnaged some css and got the player to move around

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 
 this is my seconds attempts at making a maze type game, i was not happy with how my first one was going and found some usefull videos on how to make it better.
 
+the main goal of my game is to create a maze and have a character move around and find the exit.
--- index.js
+++ index.js
@@ -13,8 +13,44 @@ image.src = 'assets/GameMap.png'
 const playerImage = new Image()
 playerImage.src = 'assets/playerDown.png'
 
-image.onload = () => {
-    context.drawImage(image, -600, -1020)
+
+class Sprite {
+    constructor({position, velocity, image}) {
+        this.position = position
+        this.image = image
+    }
+
+    draw() {
+        context.drawImage(this.image, this.position.x, this.position.y)
+    }
+}
+
+const background = new Sprite({
+    position: {
+        x: -600,
+        y: -1012
+    },
+    image: image
+})
+
+const keys = {
+    w: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    }
+}
+
+function animate() {
+    window.requestAnimationFrame(animate)    
+    background.draw()
     context.drawImage(
         playerImage,
         0,
@@ -26,29 +62,47 @@ image.onload = () => {
         playerImage.width / 4,
         playerImage.height
     )
-}
 
-function animate() {
-    window.requestAnimationFrame()
-    console.log('animate')
+    if (keys.w.pressed) background.position.y = background.position.y + 3
+    else if (keys.a.pressed) background.position.x = background.position.x + 3
+    else if (keys.s.pressed) background.position.y = background.position.y - 3
+    else if (keys.d.pressed) background.position.x = background.position.x - 3
 }
 animate()
 
 window.addEventListener('keydown', (e) => {
     switch (e.key) {
         case 'w':
-            console.log('pressed w key')
+        keys.w.pressed = true
             break
         case 'a':
-            console.log('pressed a key')
+        keys.a.pressed = true
             break
         case 's':
-            console.log('pressed s key')
+        keys.s.pressed = true
             break
         case 'd':
-            console.log('pressed d key')
+        keys.d.pressed = true
             break
     }
+    console.log(keys)
 })
 
+window.addEventListener('keyup', (e) => {
+    switch (e.key) {
+        case 'w':
+            keys.w.pressed = false
+            break
+        case 'a':
+            keys.a.pressed = false
+            break
+        case 's':
+            keys.s.pressed = false
+            break
+        case 'd':
+            keys.d.pressed = false
+            break
+    }
+    console.log(keys)
+})
 
